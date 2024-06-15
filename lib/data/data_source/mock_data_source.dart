5ed8e2d103b77becdcb2bd5cd875efa9945d5ad7
Kyeonghwan-Kong <kyeonghwan.kong@gmail.com>
2024-06-15 16:11:41 +0900

    Update mock_data_source.dart

--- lib/data/data_source/mock_data_source.dart
+++ lib/data/data_source/mock_data_source.dart
@@ -7,12 +7,15 @@ import 'package:graph_sample/data/data_source/data_source.dart';
 import 'package:graph_sample/data/model/blood_glucose_graph_response_model.dart';
 import 'package:injectable/injectable.dart';
 
+@visibleForTesting
 const mockDataPath = 'assets/data.json';
 
 @Injectable(as: DataSource)
 class MockDataSource implements DataSource {
   @override
   Future<ResponseModel<BloodGlucoseGraphModel>> getBloodGlucoseGraph() async {
+    await Future.delayed(const Duration(seconds: 3)); // For loading test
+
     final data = await loadMockData(mockDataPath);
     return ResponseModel(data: data);
   }
