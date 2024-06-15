ce2dfd80c22ff323e314c72ce0166aa47fd03b22
Kyeonghwan-Kong <kyeonghwan.kong@gmail.com>
2024-06-15 09:47:08 +0900

    Update get_blood_glucose_graph_use_case.dart

--- lib/domain/use_case/get_blood_glucose_graph_use_case.dart
+++ lib/domain/use_case/get_blood_glucose_graph_use_case.dart
@@ -1,10 +1,17 @@
 import 'package:graph_sample/common/domain/use_case.dart';
+import 'package:graph_sample/dependency_injection.dart';
 import 'package:graph_sample/domain/entity/blood_glucose_graph_entity.dart';
+import 'package:graph_sample/domain/repository/blood_glucose_repository.dart';
+import 'package:injectable/injectable.dart';
 
+@Injectable()
 class GetBloodGlucoseGraphUseCase implements UseCase<Future<BloodGlucoseGraphEntity>, void> {
+  final BloodGlucoseRepository _repository;
+
+  GetBloodGlucoseGraphUseCase([BloodGlucoseRepository? repository]) : _repository = repository ?? inject();
+
   @override
-  Future<BloodGlucoseGraphEntity> execute(void param) {
-    // TODO: implement execute
-    throw UnimplementedError();
+  Future<BloodGlucoseGraphEntity> execute([void param]) {
+    return _repository.getBloodGlucoseGraphEntity();
   }
 }
